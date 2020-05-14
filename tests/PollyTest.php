95056e700351b9e8c02f795ab515dd5dcd88215b
Rigel Kent Carbonel <rigel20.kent@gmail.com>
2020-05-14 16:27:24 +0800

    remove testsample

--- tests/PollyTest.php
+++ tests/PollyTest.php
@@ -13,20 +13,4 @@ class PollyTest extends PollyTestCase
 
         $this->assertInstanceOf(\Aws\Polly\PollyClient::class, $client);
     }
-
-    /** @test */
-    public function testSample()
-    {
-        $client = AmazonPolly::getClient();
-
-        $test = $client->synthesizeSpeech([
-            'Engine' => 'standard',
-            'OutputFormat' => 'mp3', // REQUIRED
-            'Text' => 'test', // REQUIRED
-            'TextType' => 'text',
-            'VoiceId' => 'Amy', // REQUIRED
-        ]);
-
-        dd($test->get('AudioStream')->getContents());
-    }
 }
