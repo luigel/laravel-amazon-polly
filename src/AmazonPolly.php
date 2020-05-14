ad87aab66c2ca51ead1c5a7a6fc3024960f287fd
Rigel Kent Carbonel <rigel20.kent@gmail.com>
2020-05-14 16:24:19 +0800

    Moved facade

--- src/AmazonPolly.php
+++ src/AmazonPolly.php
@@ -4,5 +4,36 @@ namespace Luigel\AmazonPolly;
 
 class AmazonPolly
 {
-    // Build your next great package.
+    /**
+     * @var \Aws\Polly\PollyClient
+     */
+    private $client;
+
+    public function __construct(array $config)
+    {
+        $credentials = $this->getCredentials($config['credentials']);
+
+        $this->client = new \Aws\Polly\PollyClient(['version' => $config['version'], 'credentials' => $credentials, 'region' => $config['region']]);
+    }
+
+    /**
+     * Get credentials of AWS
+     *
+     * @param array $credentials
+     * @return \Aws\Credentials\Credentials
+     */
+    protected function getCredentials(array $credentials)
+    {
+        return new \Aws\Credentials\Credentials($credentials['key'], $credentials['secret']);
+    }
+
+    /**
+     * Get the Polly Client
+     *
+     * @return \Aws\Polly\PollyClient
+     */
+    public function getClient(): \Aws\Polly\PollyClient
+    {
+        return $this->client;
+    }
 }
